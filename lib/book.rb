7ff3027623865778b4b91bb8ae67102540d0f2c3
katieroseschulz <katiekaterose13@gmail.com>
2018-07-26 20:20:33 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
-# book.rb
-# Add you Book class here
+
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+end
+book = Book.new("The World According to Garp")
+book.turn_page 
